fa82cb21d032010bdfe44b79f9794898125e4c75
SachinKonan <sachinkonan480@gmail.com>
2017-01-21 16:48:15 -0700

    added files

---
+++ DAC/iheritance/oscilloscope2.py
@@ -0,0 +1,46 @@
+import serial
+import matplotlib.pyplot as plt
+from drawnow import *
+import atexit
+from ADS import ADCThread
+
+def plotValues():
+    plt.title('Serial value from Arduino')
+    plt.grid(True)
+    plt.ylabel('Values')
+    plt.plot(values, 'rx-', label='values')
+    plt.legend(loc='upper right')
+
+def doAtExit():
+    adc.stop()
+    print("ADS115 is " + adc.interrupt)
+values = []
+
+plt.ion()
+cnt=0
+
+adc = ADCThread(0x48)
+
+atexit.register(doAtExit)
+
+print("ADS is open: " )
+
+#pre-load dummy data
+for i in range(0,26):
+    values.append(0)
+
+while True:
+    valueRead = adc.getADCVal1()
+
+    #check if valid value can be casted
+    try:
+        valueInInt = int(valueRead)
+        print(valueInInt)
+        if valueInInt >= 0:
+            values.append(valueInInt)
+            values.pop(0)
+            drawnow(plotValues)
+            else:
+                print("Invalid! negative number")
+    except ValueError:
+        print("Invalid! cannot cast")
